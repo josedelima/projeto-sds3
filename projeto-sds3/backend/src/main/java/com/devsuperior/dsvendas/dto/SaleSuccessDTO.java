c76980de5aefdd6bb77f76505046e577a88344f0
José Augusto Rodrigues de Lima <josedelimajf@gmail.com>
2021-05-08 21:44:23 -0300

    Group by search

---
+++ projeto-sds3/backend/src/main/java/com/devsuperior/dsvendas/dto/SaleSuccessDTO.java
@@ -0,0 +1,49 @@
+package com.devsuperior.dsvendas.dto;
+
+import java.io.Serializable;
+
+import com.devsuperior.dsvendas.entities.Saller;
+
+public class SaleSuccessDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private String sallerName;
+	private Long visited;
+	private Long deals;
+	
+	
+	public SaleSuccessDTO() {}
+
+	public SaleSuccessDTO(Saller saller, Long visited, Long deals) {
+		this.sallerName = saller.getName();
+		this.visited = visited;
+		this.deals = deals;
+	}
+
+	public String getSallerName() {
+		return sallerName;
+	}
+
+	public void setSallerName(String sallerName) {
+		this.sallerName = sallerName;
+	}
+
+	public Long getVisited() {
+		return visited;
+	}
+
+	public void setVisited(Long visited) {
+		this.visited = visited;
+	}
+
+	public Long getDeals() {
+		return deals;
+	}
+
+	public void setDeals(Long deals) {
+		this.deals = deals;
+	}
+	
+	
+
+}
